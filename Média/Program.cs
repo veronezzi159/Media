e53b8b43ab0ae50501ad5a3390f36a147eb5c33c
L.Veronezzi <L.Veronezzi@DESKTOP-IQ2PR1Q>
2024-04-19 00:47:07 -0300

    Add project files.

---
+++ Média/Program.cs
@@ -0,0 +1,24 @@
+﻿decimal n1, n2, media;
+
+Console.WriteLine("Dgite a primeira nota ");
+n1 = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Dgite a segunda nota ");
+n2 = decimal.Parse(Console.ReadLine());
+
+media = (n1 + n2) / 2;
+
+if (media > 0)
+{
+    if (media <= 3)
+    {
+        Console.WriteLine("Reprovado");
+    }
+    else if (media > 3 && media <=5 )
+    {
+        Console.WriteLine("Aluno de exame");
+    } else
+    {
+        Console.WriteLine("Aprovado");
+    }
+}
